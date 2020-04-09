f1228baa04d856a74dc4a951e20dedb32284d45e
haus2013@yandex.ru <goglechrome1>
2020-04-09 17:23:46 +0500

    move to the future

--- bot.py
+++ bot.py
@@ -1,41 +1,55 @@
-import config
 import discord
-import pymysql
+import sqlite3
 import asyncio
-import os
 import pytz
 from datetime import datetime
 
 client = discord.Client()
-
-db = pymysql.connect('31.31.196.157', config.LOG, config.PASS, config.DB)
-
+db = sqlite3.connect("discord.db")
 tz = pytz.timezone('Asia/Yekaterinburg')
 
 @client.event
+async def on_connect():
+    print('Client connect to discord!')
+
+@client.event
 async def on_ready():
-    print('Bot ready')
     for guild in client.guilds:
-        for member in guild.members:
-            if member.id != 693871698298142721:
-                cursor = db.cursor()
-                sql = "INSERT INTO users (dis_id, dis_nick) VALUES (%s, %s)"
-                try:
-                    cursor.execute(sql, (str(member.id), str(member)))
-                    db.commit()
-                except Exception as e:
-                    print("В запросе возникла ошибка !"+'\n'+str(e))
-                    db.rollback()
+        print('    Bot ready on <<' + guild.name + '>> server')
+        cursor = db.cursor()
+        sql = """CREATE TABLE IF NOT EXISTS `%s` (
+            `u_id` INTEGER PRIMARY KEY AUTOINCREMENT, 
+            `dis_id` int(20) UNIQUE, 
+            `gay_role` boolean DEFAULT 0, 
+            `gay_count` int(11) DEFAULT 0 );
+            """ % guild.id
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except Exception as e:
+            print (e)
+        else:
+            for member in guild.members:
+                if member.bot == False:
+                    sql = """INSERT INTO `%s` (dis_id) VALUES (?)""" % guild.id
+                    try:
+                        cursor.execute(sql, (member.id,))
+                        db.commit()
+                    except Exception:
+                        pass
+        finally:
+            cursor.close()
 
 @client.event 
 async def on_message(message):
+    channel = message.channel
+
     if message.author == client.user:
         return
 
     if message.content.startswith('!who'):
-        channel = message.channel
         cursor = db.cursor()
-        sql = "SELECT dis_id FROM users WHERE gay_role = 1 LIMIT 1"
+        sql = """SELECT `dis_id` FROM `%s` WHERE `gay_role` = 1 LIMIT 1""" % message.guild.id
         try:
             cursor.execute(sql)
             result = cursor.fetchone()
@@ -46,12 +60,13 @@ async def on_message(message):
         except Exception as e:
             print("Ошибка при выполнении запроса: " + str(e))
             db.rollback()
+        finally:
+            cursor.close()
 
     if message.content.startswith('!top'):
         toplist=[]
-        channel = message.channel
         cursor = db.cursor()
-        sql = "SELECT dis_id, gay_count FROM users ORDER BY gay_count DESC LIMIT 10"
+        sql = "SELECT `dis_id`, `gay_count` FROM `%s` ORDER BY `gay_count` DESC LIMIT 10" % message.guild.id
         try:
             cursor.execute(sql)
             results = cursor.fetchall()
@@ -59,66 +74,57 @@ async def on_message(message):
                 dis_id = row[0]
                 gay_count = row[1]
                 toplist.append("<@" + str(dis_id) +"> был пидорасом "+ str(gay_count) + " раз\n")
-
             await channel.send(''.join(toplist))
         except Exception as e:
             print("Ошибка при выполнении запроса: " + str(e))
             db.rollback()
+        finally:
+            cursor.close()
 
     if message.content.startswith('!help'):
-        channel = message.channel
         emb = discord.Embed(title = "гейские команды", colour = 0xffc0cb)
         emb.add_field(name="!help", value="Показывает это сообщение")
         emb.add_field(name="!who", value="Показать кто сйечас пидор")
         emb.add_field(name="!top", value="Показать топ 10 пидоров")
         await channel.send(embed=emb)
 
-
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(config.CHANNEL_ID)
-    cursor = db.cursor()
-    sql = "INSERT INTO users (dis_id, dis_nick) VALUES (%s, %s)"
-    try:
-        cursor.execute(sql, (str(member.id), str(member)))
-        db.commit()
-        await channel.send("Приветствуем тебя <@{0}> на нашем НЕ гетероСЕКСУАЛЬНОМ кАНАЛЕ".format(member.id))
-    except Exception as e:
-        print(e)
+    for TextChannel in member.guild.text_channels:
+        await TextChannel.send("Приветствуем тебя <@{0}>".format(member.id))
+        break
 
 @client.event
 async def on_member_remove(member):
-    channel = client.get_channel(config.CHANNEL_ID)
-    cursor = db.cursor()
-    sql = "DELETE FROM users WHERE dis_id = %s OR dis_nick = %s"
-    try:
-        cursor.execute(sql, (str(member.id), str(member)))
-        db.commit()
-        await channel.send("Нас покидает <@{0}> ...................... похуй......🕯".format(member.id))
-    except Exception as e:
-        print(e)
+    for TextChannel in member.guild.text_channels:
+        await TextChannel.send("Попрощаемся с <@{0}>".format(member.id))
+        break
 
 async def get_gay():
     await client.wait_until_ready()
-    channel = client.get_channel(config.CHANNEL_ID)
-    timer = datetime(year = 2020, month=12, day=31, hour = 12, minute = 0, second = 0).strftime('%X')
+    await asyncio.sleep(2)
+    timer = datetime(year = 2020, month=12, day=31, hour = 10, minute = 0, second = 0).strftime('%X')
     while not client.is_closed():
         now = datetime.now(tz).strftime('%X')
         if now == timer:
             try:
                 cursor = db.cursor()
-                sql = "UPDATE users SET gay_role = 0 WHERE gay_role = 1"
-                cursor.execute(sql)
-                sql = "SELECT dis_id FROM users WHERE dis_id != 693871698298142721 ORDER BY RAND() LIMIT 1;"
-                cursor.execute(sql)
-                result = cursor.fetchone()
-                sql = "UPDATE users SET gay_role = 1 , gay_count = gay_count + 1 WHERE dis_id = %s"
-                cursor.execute(sql, str(result[0]))
-                db.commit()
-                await channel.send("Пидорас дня => <@" + str(result[0]) + ">")
+                for guild in client.guilds:
+                    sql = "UPDATE `%s` SET `gay_role` = 0 WHERE `gay_role` = 1" % guild.id
+                    cursor.execute(sql)
+                    sql = "SELECT `dis_id` FROM `%s` ORDER BY RANDOM() LIMIT 1;" % guild.id
+                    cursor.execute(sql)
+                    result = cursor.fetchone()
+                    sql = "UPDATE `%s` SET `gay_role` = 1 , `gay_count` = `gay_count` + 1 WHERE `dis_id` = %s" % (guild.id, str(result[0]))
+                    cursor.execute(sql)
+                    db.commit()
+                    for TextChannel in guild.text_channels:
+                        await TextChannel.send("Пидорас дня => <@" + str(result[0]) + ">")
+                        break
             except Exception as e:
                 print(e)
-            await asyncio.sleep(60)
+            finally:
+                await asyncio.sleep(86395)
         await asyncio.sleep(1)
 
 client.bg_task = client.loop.create_task(get_gay())
--- config.py
+++
@@ -1,9 +0,0 @@
-CHANNEL_ID = 424634837870051341
-
-HOST = 'localhost'
-
-LOG = 'u0954975_niddle'
-
-PASS = 'goglechrome1'
-
-DB = 'u0954975_discord'
