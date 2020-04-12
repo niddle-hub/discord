7e7b7b517710454f3e596b9069835236f93463e6
haus2013@yandex.ru <goglechrome1>
2020-04-12 16:40:20 +0500

    upd

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ async def on_member_remove(member):
 async def get_gay():
     await client.wait_until_ready()
     await asyncio.sleep(2)
-    timer = datetime(year = 2020, month=12, day=31, hour = 16, minute = 40, second = 0).strftime('%X')
+    timer = datetime(year = 2020, month=12, day=31, hour = 16, minute = 42, second = 0).strftime('%X')
     while not client.is_closed():
         now = datetime.now(tz).strftime('%X')
         if now == timer:
